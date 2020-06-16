68c6614fd80071f83eee2c8d326575b99a7ca4d8
Alexander Radchenko <48209253+radchenko511@users.noreply.github.com>
2020-06-16 11:56:09 -0700

    jenk_start_test

--- test.py
+++ test.py
@@ -21,17 +21,17 @@ def test_site_login_chrome():
     driver = webdriver.Remote(
         command_executor="http://128.199.103.130:4444/wd/hub",
         desired_capabilities=capabilities)
-    driver.implicitly_wait(10)
 
     driver.get('http://at.dev01.1iu.ru')
+    driver.implicitly_wait(10)
 
     driver.find_element_by_xpath("//li[3]//a[1]").click()
     print("Кликнули")
     driver.find_element_by_name("email").send_keys(login_at)
     driver.find_element_by_name("password").send_keys(pass_at)
     driver.find_element_by_xpath("//button[@class='btn modal__btn']").click()
-    driver.implicitly_wait(10)
+    #driver.implicitly_wait(10)
     driver.get('https://at.dev01.1iu.ru/account/profile')
-    driver.implicitly_wait(10)
+    #driver.implicitly_wait(10)
     assert driver.find_element_by_xpath("//input[@id='email']").get_attribute("value") == login_at
     driver.quit()
