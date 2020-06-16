44980d84f8c5b7e28dbf67071818fd76e7686d57
Alexander Radchenko <48209253+radchenko511@users.noreply.github.com>
2020-06-16 11:17:51 -0700

    jenk_start_test

--- test.py
+++ test.py
@@ -30,8 +30,8 @@ def test_site_login_chrome():
     driver.find_element_by_name("email").send_keys(login_at)
     driver.find_element_by_name("password").send_keys(pass_at)
     driver.find_element_by_xpath("//button[@class='btn modal__btn']").click()
-#    time.sleep(2)
+    time.sleep(2)
     driver.get('https://at.dev01.1iu.ru/account/profile')
-#    time.sleep(2)
+    time.sleep(2)
     assert driver.find_element_by_xpath("//input[@id='email']").get_attribute("value") == login_at
     driver.quit()
