7ce073fd4a49bcd12d656fae9c9cdd8693af6989
Alexander Radchenko <48209253+radchenko511@users.noreply.github.com>
2020-07-19 13:34:31 -0700

    jenk_start_test

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ def test_site_login_chrome(browser):
     browser.find_element_by_name("password").send_keys(pass_at)
     browser.find_element_by_xpath("//button[@class='btn modal__btn']").click()
     assert browser.find_element_by_link_text("Создать курс в папке").is_displayed() == True
-    allure.attach("engine.title", browser.get_screenshot_as_png(), attachment_type=AttachmentType.PNG)
+    allure.attach("engine.title", browser.get_screenshot_as_png())
 
 @pytest.mark.order2
 def test_site_create_cours(browser):
