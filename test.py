62fd57859614e4455353cd5998775230669f5fc3
Alexander Radchenko <48209253+radchenko511@users.noreply.github.com>
2020-07-19 21:52:35 -0700

    jenk_start_test

--- test.py
+++ test.py
@@ -28,7 +28,7 @@ def test_site_test(browser):
         a1 = Image.open(file_1)
         b1 = Image.open(file_2)
         c = image_1.graphic_difference(a1, b1)
-        # c.save('333.png')
+        c.save('333.png')
         allure.attach.file("333.png", attachment_type=allure.attachment_type.PNG)
 
     # assert int(image_1.percentage_difference(file_1, file_2)) == 0
