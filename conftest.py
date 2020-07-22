318ce612549e11c1144d607ca2757d4b59fd0c61
Alexander Radchenko <48209253+radchenko511@users.noreply.github.com>
2020-07-21 17:21:13 -0700

    jenk_start_test

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ import allure
 def browser():
     capabilities = remote_driver.browser
     driver = webdriver.Remote(
-        command_executor=remote_driver.ip_selenoid_mac,
+        command_executor=remote_driver.ip_selenoid,
         desired_capabilities=capabilities)
     driver.maximize_window()
     driver.implicitly_wait(10)
